19a42cc42fbc2b3d765f14521f398912009ba2e1
promologistik-art <promologistik@gmail.com>
2026-05-27 12:35:08 +0400

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,56 +1,95 @@
 from .constants import (
-    AWAITING_SOURCE_USERNAME, AWAITING_TARGET_FORWARD, AWAITING_CRITERIA,
-    AWAITING_INTERVAL, AWAITING_VIEWS, AWAITING_REACTIONS, AWAITING_SIGNATURE,
-    AWAITING_POST_INTERVAL, AWAITING_POST_START_TIME,
-    AWAITING_MEDIA_FILTER, AWAITING_REMOVE_TEXT,
-    AWAITING_TARIFF_SELECT, AWAITING_BROADCAST_MESSAGE,
-    AWAITING_EDIT_VIEWS, AWAITING_EDIT_REACTIONS, AWAITING_EDIT_EXCLUDE_PHRASES,
-    AWAITING_KEYWORDS, AWAITING_EDIT_KEYWORDS,
+    AWAITING_SOURCE_USERNAME,
+    AWAITING_TARGET_FORWARD,
+    AWAITING_CRITERIA,
+    AWAITING_INTERVAL,
+    AWAITING_VIEWS,
+    AWAITING_REACTIONS,
+    AWAITING_SIGNATURE,
+    AWAITING_POST_INTERVAL,
+    AWAITING_POST_START_TIME,
+    AWAITING_MEDIA_FILTER,
+    AWAITING_REMOVE_TEXT,
+    AWAITING_TARIFF_SELECT,
+    AWAITING_BROADCAST_MESSAGE,
+    AWAITING_EDIT_VIEWS,
+    AWAITING_EDIT_REACTIONS,
+    AWAITING_EDIT_EXCLUDE_PHRASES,
+    AWAITING_KEYWORDS,
+    AWAITING_EDIT_KEYWORDS,
     # YouTube states
-    AWAITING_YOUTUBE_SOURCE_TYPE, AWAITING_YOUTUBE_CHANNEL_ID,
-    AWAITING_YOUTUBE_LINK, AWAITING_YOUTUBE_SEARCH_QUERY,
-    AWAITING_YOUTUBE_COUNTRY, AWAITING_YOUTUBE_CATEGORY,
+    AWAITING_YOUTUBE_SOURCE_TYPE,
+    AWAITING_YOUTUBE_CHANNEL_ID,
+    AWAITING_YOUTUBE_LINK,
+    AWAITING_YOUTUBE_SEARCH_QUERY,
+    AWAITING_YOUTUBE_COUNTRY,
+    AWAITING_YOUTUBE_CATEGORY,
     AWAITING_YOUTUBE_CONTENT_TYPE
 )
 
 from .common import start, help_command, cancel
 from .projects import (
-    my_projects, projects_callback, project_menu_callback, handle_project_name,
-    back_to_projects_callback, show_project_stats
+    my_projects,
+    projects_callback,
+    project_menu_callback,
+    handle_project_name,
+    back_to_projects_callback,
+    show_project_stats
 )
 from .sources import (
-    add_source_start, youtube_source_type_callback,
-    youtube_channel_id_input, youtube_link_input, youtube_search_query_input,
-    youtube_country_callback, youtube_category_callback,
+    add_source_start,
+    youtube_source_type_callback,
+    youtube_channel_id_input,
+    youtube_link_input,
+    youtube_search_query_input,
+    youtube_country_callback,
+    youtube_category_callback,
     youtube_content_type_callback,
-    add_source_criteria, criteria_views_input, criteria_reactions_input,
-    media_filter_callback, duration_callback, remove_text_callback,
-    add_keywords_yes_callback, add_keywords_skip_callback, process_keywords_input,
-    my_sources, edit_source_callback,
+    my_sources,
+    edit_source_callback,
+    delete_source_callback,
+    confirm_delete_source_callback,
+    cancel_delete_source_callback,
     back_to_sources_callback
 )
 from .targets import (
-    add_target_start, add_target_forward, add_target_continue_callback,
-    my_targets, delete_target_callback
+    add_target_start,
+    add_target_forward,
+    add_target_continue_callback,
+    my_targets,
+    delete_target_callback
 )
 from .settings import (
-    set_interval_start, set_interval_callback,
-    set_post_interval_start, set_post_interval_callback,
+    set_interval_start,
+    set_interval_callback,
+    set_post_interval_start,
+    set_post_interval_callback,
     set_post_start_time_callback,
-    set_signature_start, set_signature_input,
-    set_interval_start_callback, set_post_interval_start_callback,
+    set_signature_start,
+    set_signature_input,
+    set_interval_start_callback,
+    set_post_interval_start_callback,
     set_signature_start_callback
 )
 from .stats import status, project_stats
 from .parsing import (
-    parse_now, queue_status, post_now,
-    clear_old_queue, clear_failed_queue, clear_all_queue, clear_project_queue,
+    parse_now,
+    queue_status,
+    post_now,
+    clear_old_queue,
+    clear_failed_queue,
+    clear_all_queue,
+    clear_project_queue,
     reset_history
 )
 from .admin import (
-    admin_panel, admin_callback, admin_back_callback,
-    admin_set_tariff_start, admin_extend_trial_start,
-    broadcast_start, broadcast_send
+    admin_panel,
+    admin_callback,
+    admin_back_callback,
+    admin_set_tariff_start,
+    admin_extend_trial_start,
+    broadcast_start,
+    broadcast_send
 )
 from .test import test_scraper, debug_reactions
 from .utils import setup_bot_commands
